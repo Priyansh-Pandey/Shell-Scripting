28a97135792663adf3ea9dc2455d4d470f9fc198
priyansh <pkpandeyji28@gmail.com>
2025-02-18 11:23:05 +0530

    1st script , channing multiple command

---
+++ 1st_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+<< 'comment'
+
+Chaining the sets of command in a single script file
+comment
+
+ echo "The present working DIrectory"
+
+ pwd
+
+ echo
+
+ echo "The current user logged in"
+
+ whoami
+
+ echo
+
+ echo "The current date and time"
+
+ date
+
+ echo
+
+ echo "Number of files and folders in the presrnt working directory"
+
+ ls
+
+ echo
+
