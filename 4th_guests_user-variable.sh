b376a88bdc66b4c851e908cf4baf2ff1174decdb
priyansh <pkpandeyji28@gmail.com>
2025-02-23 12:52:11 +0530

    more scripts

---
+++ 4th_guests_user-variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+<<'comment'
+Testing User-Defined Variables
+comment
+
+days=10th
+guest=priyansh
+echo "Today is the $days and the news guest arrived $guest"
+
+echo
+
+days=5th
+guest=rome
+echo "Today is the $days and the news guest arrived $guest"
---
+++ 5th-variale-to-variabel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+<<'comment'
+Assiging a Variable value to another variable
+comment
+
+variable1=10th
+variable2=$variable1
+variable3=day
+echo "Today is the $variable2 $variable3 of this month"
