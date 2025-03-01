ff7c34f62c53b577a45b5b2d9d336c2c66d241f1
Priyansh <priyanshkumarpandey01@gmail.com>
2025-03-01 13:01:21 +0530

    old and moern bash approach

--- epoch_ex2.sh
+++ epoch_ex2.sh
@@ -2,11 +2,27 @@
 
 # This is the 2nd example of epoch feature
 
+#this is the old apparoch bash
+
+<<'comment'
 date1="Jan 1 ,2023"
 date2="May 1 ,2023"
 time1=$(date -d "Jan 1, 2023" +%s)
 time2=$(date -d "May 1, 2023" +%s)
 diff=$(($time2 -$time1))
 days=$[$diff / (24 * 60 * 60)] 
+
+echo " no of days: $days"
+comment
+
+
+#this is the modern approach in bash
+
+date1="Jan 1 ,2023"
+date2="May 1 ,2023"
+time1=$(date -d "Jan 1, 2023" +%s)
+time2=$(date -d "May 1, 2023" +%s)
+diff=$(($time2 -$time1))
+days=$(($diff / (24 * 60 * 60)))
 
 echo " no of days: $days"
